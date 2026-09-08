2fc6cdc5b3dc6a5428e66bc5a1a8c3c22b1af609
marinosialita <marino.sialita@gmail.com>
2026-09-09 01:02:53 +0300

    Activate elite AQUAPHOR intelligence application

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './styles.css';
+import EliteApp from './EliteApp';
+import './elite.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const root = document.getElementById('root');
+if (!root) throw new Error('Root element was not found');
+
+createRoot(root).render(
+  <StrictMode>
     <BrowserRouter>
-      <App />
+      <EliteApp />
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 );
